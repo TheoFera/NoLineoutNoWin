1da1c02f4905f7097ef388f3736c870833764003
TheoFera <feratheo@orange.fr>
2026-08-01 18:45:19 +0200

    Sauvegarde avant publication Play Store

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "fr.nolineout.nowin",
+  appId: "fr.partagetonjeu.nolineoutnowin",
   appName: "No Lineout No Win",
   webDir: "dist",
   plugins: {
